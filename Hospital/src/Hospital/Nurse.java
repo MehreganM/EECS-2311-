bb70b72089eaf419d78ba4d8f76acf2bc12800a1
HarrishElango <58488367+HarrishElango@users.noreply.github.com>
2024-02-08 20:20:35 -0500

    Update Nurse.java
    Added some needed and possible future functions for the Nurse user type.

--- Hospital/src/Hospital/Nurse.java
+++ Hospital/src/Hospital/Nurse.java
@@ -1,5 +1,81 @@
 package Hospital;
 
-public class Nurse {
+public class Nurse extends Employee {
+	
+	protected ArrayList<Patient> patients= new ArrayList<Patient>();
+	
+	public boolean addPatient(Patient patient) {
+		if(patients.size()<15) {
+			patients.add(patient);
+			patient.setNurse(this);
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public List<Patient> extractPatientDetail() {
+		//we make a tree set to sort the patients because it uses compareTo
+		//we have already defined compareTo
+		Set<Patient> patientTree = new TreeSet<>();
+		for(int i = 0; i<patients.size();i++) {
+		//	String temp= physicianList.get(i).firstName+ ", "+ physicianList.get(i).lastName;
+			patientTree.add(patients.get(i));
+		}
+		//we add it to a list because we should return a list
+		List<Patient> extracted = new ArrayList<Patient>();
+		extracted.addAll(patientTree);
+		return extracted;
+	}
+	
+	public String currentMeds(Patient patient) {
+		String meds = getMeds(patient); // no getMeds function but this may change if we are using a DB
+		return meds;
+		//this is temporary as we may save it as a concatenated string or we might
+		//to pull this information from a database which will require the code to change
+	}
+	
+	public String getMDNotes(Physician phys) {
+		//this will have to be related to a database
+		//we will have to retrieve the information from said database
+	}
+	
+	
+	
+	public boolean equals(Object object) { // this way we can see if this nurse is the same as another
+		//or to see if the thing we are comparing the nurse to is of object type Nurse
+		if(this==object) {
+			return true;
+		}
+		if(object==null) {
+			return false;
+		}
+		if(getClass()!=object.getClass()) {
+			return false;
+		}
+		Nurse other=(Nurse) object;
+		if(this.compareTo(other)==0) {
+			return true;
+		}
+		return false;
+	}
+	
+	
+	public int compareTo(Nurse other) { // this is another way to compare nurses to see, likely related
+		//to hiring more nurses for the hospital or making sure that we don't have duplicates
+		
+		if(other!=null) {
+			if(this.firstName.equals(other.firstName)&&this.lastName.equals(other.lastName)&&this.age==other.age&&this.gender.equals(other.gender)&&this.address.equals(other.address)&&this.salary==other.salary&&this.specialty.equals(other.specialty)) {
+				return 0;
+			}
+			else {
+				return this.getName().compareTo(other.getName());
+			}			
+		}
+		else {
+			return 2;
+		}
+	}
 
 }

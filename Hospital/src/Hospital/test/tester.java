6127dc29af47709763a2fe466d2cc920a035b1d8
Mehregan <mehregan@my.yorku.ca>
2024-02-14 16:47:24 -0500

    fix of test file

--- Hospital/src/Hospital/test/tester.java
+++ Hospital/src/Hospital/test/tester.java
@@ -1,7 +1,6 @@
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import Hospital.*;
-
 
 public class HospitalTest {
     private Hospital hospital;

4d4dcc12b4127a1665d302313a9bfff8a101df04
amira72 <157905644+amira72@users.noreply.github.com>
2024-02-16 20:39:59 -0500

    Update 2 Physician.java
    Added more methods: 
    - Get and set methods for the physician administrator
    - extractPatientDetail() method
    - hasMaximumPatients() method

--- Hospital/src/Hospital/Physician.java
+++ Hospital/src/Hospital/Physician.java
@@ -1,19 +1,20 @@
 package Hospital;
 
+package eecs2311;
+
 import java.util.ArrayList;
-
-
-import java.util.Objects;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 
 /**
- * this class is Physician which extends SalariedEmployee and implements Comparable
- * and stores the physician's specialty and an arrayList of their patients
- * and the physician administrator that has their specialty and also a list of volunteers
- * @author Amira
+ * This class is Physician which extends Employee and stores the physician's attributes
+ * and their specialty. It also has an arrayList of the physician's patients, volunteers, 
+ * and nurses and the physician administrator.
+ * @author Amira Mohamed
  *
  */
-
 public class Physician extends Employee {
 	
 	String specialty;
@@ -24,7 +25,7 @@ public class Physician extends Employee {
 
 	
 	/**
-	 * This is the default constructor that creates a physician but does not initialize the physician's attributes.
+	 * This is the default constructor that creates a physician but does not initializes the attributes of the physician.
 	 */
 	public Physician() {
 		super();
@@ -35,7 +36,7 @@ public class Physician extends Employee {
 	 * first name, last name, age, gender, and address. 
 	 * @param firstName is the first name of the physician that is stored as a string
 	 * @param lastName is the last name of the physician that is stored as a string
-	 * @param age is the age of the physician that is stored as an integer
+	 * @param age is the age of the physician that is stored as a integer
 	 * @param gender is the gender of the physician that is stored as a string
 	 * @param address is the address of the physician that is stored as a string
 	 */
@@ -56,15 +57,15 @@ public class Physician extends Employee {
 	
 	
 	/**
-	 * This method sets the specialty of the physician. It takes in a string as a parameter and sets it as the 
+	 * This method sets the specialty of the physician. It takes in a string as a parameter and set it as the 
 	 * physician's specialty if it is one of these: Radiology, Dermatology, Neurology, Cardiology, Hematology, and Immunology. 
 	 * @param specialty is the input parameter as a string. 
 	 * The method checks if the input is one of the above specialties. If so, we assign it as the physician's specialty, otherwise 
-	 * we throw an IllegalArgumentException.
+	 * we throws an IllegalArgumentException.
 	 * @throws IllegalArgumentException if the input string is none of the specified specialties.
 	 */
 	
-	public void setSpeciality(String specialty) throws IllegalArgumentException {
+	public void setSpecialty(String specialty) throws IllegalArgumentException {
 		if (specialty.equals("Radiology")||specialty.equals("Dermatology")||specialty.equals("Neurology")||
 				specialty.equals("Cardiology") || specialty.equals("Hematology") || specialty.equals("Immunology")){
 			this.specialty = specialty;
@@ -79,12 +80,12 @@ public class Physician extends Employee {
 	 * in the list is less than 10. The method takes in the patient as an input parameter and returns 
 	 * true if the patient was successfully added to the physician's patient list. 
 	 * The physician can have a maximum of 10 patients.
-	 * @param patient is an object of type Patient that needs to be assigned a physician
+	 * @param patient is an object of Patient type that needs to be assign a physician
 	 * @return true if the patient was successfully assigned a physician and false otherwise. 
 	 */
 	
 	public boolean addPatient (Patient patient) {
-		if (patient.getSize() < 10) {
+		if (patients.size() < 10) {
 			patients.add(patient);
 			patient.setPhysician(this);
 			return true;
@@ -115,10 +116,56 @@ public class Physician extends Employee {
 		}
 		return false;
 	}
+	
+	
+	/**
+	 * This method sets the physician administrator for the physician
+	 * @param admin is the the physician administrator that we want to set
+	 */
+	public void setAdmin(PhysicianAdminstrator admin) {
+		this.admin = admin;
+	}
+	
+	/**
+	 * This is the getter method to get the admin of the physician 
+	 * @return it returns the admin of the physician
+	 */
+	
+	public PhysicianAdminstrator getAdmin() {
+		return admin;
+	}
 
 	/**
-	 * This method prints the string representation of the physician's attributes.
-	 * @return it returns the physician's first name, last name, employee ID, age, gender, address, and specialty
+	 * This method is to extract patient information from the system. 
+	 * @return it returns the list of the patients in the hospital.
+	 */
+	public List<Patient> extractPatientDetail(){
+		Set<Patient> patientTree = new TreeSet<>();
+		for(int i = 0; i<patients.size();i++) {
+			patientTree.add(patients.get(i));
+		}
+		//we add it to a list to return it as a list
+		List<Patient> PatientInfo = new ArrayList<Patient>();
+		PatientInfo.addAll(patientTree);
+		return PatientInfo;
+		
+	}
+	
+	
+	/**
+	 * This method is to check whether the physician has the maximum number of patients or not. 
+	 * It return true if the patients in the list are exactly 10 patients, and false otherwise. 
+	 * @return an indication about the number of patients in the physician's list. It is true if 
+	 * they are 10, and false otherwise.  
+	 */
+	public boolean hasMaximumPatients() {
+		return (patients.size() == 10);
+		
+	}
+	
+	/**
+	 * This method print the string representation of the physician's attributes.
+	 * @return it returns the physisican's first name, last name, employee ID, age, gender, address, and specialty
 	 */
 	@Override
 	public String toString() {

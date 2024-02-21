8e5d46ca8b31ef4762a046046b82952c01a7b2a4
Mehregan <mehregan@my.yorku.ca>
2024-02-21 18:38:55 -0500

    labratory class

--- Hospital/src/Hospital/labratory.java
+++ Hospital/src/Hospital/labratory.java
@@ -1,30 +1,58 @@
 
 import java.util.ArrayList;
 import java.util.List;
+
+import hospital.Patient;
+import hospital.labTest;
 	
 public class labratory {
 
 
-
-	public class Laboratory {
-	    private List<TestRequest> testRequests;
+	public static class Laboratory {
+	    private List<labTest> testRequests;
 
 	    public Laboratory() {
 	        this.testRequests = new ArrayList<>();
 	    }
 
-	    public boolean addTestRequest(TestRequest request) {
+	    public boolean addTestRequest(labTest request) {
 	        return testRequests.add(request);
 	    }
 
-	    public TestResult processTestRequest(TestRequest request) {
-
-	        return new TestResult(request, "Result Details");
+	    public String testResults(Patient patient, String type) {
+	        for (labTest request : testRequests) {
+	            if (request.getPatient().equals(patient) && request.getType().equals(type)) {
+	                return request.getResults();
+	            }
+	        }
+	        return null; 
 	    }
 	}
 
+}
 
+class labTest{
+	private Patient patient; 
+	private String type; 
+	private String results; 
 	
-
+	  public labTest(Patient patient, String type) {
+	        this.patient = patient;
+	        this.type = type;
+	    }
+	public String getResults() {
+		return this.results;
+	}
+	public String getType() {
+		return this.type;
+	}
+	public Patient getPatient() {
+		return this.patient;
+	}
+	public void addResult(String result) {
+		this.results = result; 
+		
+	}
+	
 }
 

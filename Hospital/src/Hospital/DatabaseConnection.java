b79498e9ea7d33859d5650609187b0f584c60395
HarrishElango <58488367+HarrishElango@users.noreply.github.com>
2024-02-27 20:49:58 -0500

    Update DatabaseConnection.java

--- Hospital/src/Hospital/DatabaseConnection.java
+++ Hospital/src/Hospital/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:postgresql://localhost/mydatabase";
+    private static final String URL = "jdbc:postgresql://localhost/mydatabase"; // this may also need to be changed to work on existing computer
     private static final String USER = "postgres"; // will need to change this to make sure it coincides with username of system using this application
     private static final String PASSWORD = "harrish5"; // will need to change this to make sure it coincides with password of system using this application
 

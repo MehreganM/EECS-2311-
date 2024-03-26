50a7b140ceefec4c7a192e1852414419cb6b43a7
amira72 <157905644+amira72@users.noreply.github.com>
2024-03-26 16:43:49 -0400

    Add files via upload
    Discharge patient GUI

---
+++ Hospital/DischargePatientGUI.java
@@ -0,0 +1,119 @@
+package Hospital;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+
+public class DischargePatientGUI extends JFrame {
+	JComboBox<String> patientsList;
+	JButton dischargeButton;
+	Hospital hospital; 
+	DatabaseOps databaseOps = new DatabaseOps();
+	
+	public DischargePatientGUI(Hospital hospital) {
+		this.hospital = hospital;
+		setTitle("Discharge Patient");
+		setSize(700, 300);
+		setLocationRelativeTo(null);
+		setLayout(new FlowLayout());
+		
+		// Creating the button and the ComboBox
+		patientsList = new JComboBox<String>();
+		populatePatientsComboBox();
+		
+		dischargeButton = new JButton("Discharge Selected Patient");
+		dischargeButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				removePatient();
+			}
+		});
+		
+		add(patientsList);
+		add(dischargeButton);
+		
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setVisible(true);
+		
+	}
+	
+	private void populatePatientsComboBox() {
+		patientsList.removeAllItems();
+		ArrayList<Patient> patients = databaseOps.getAllPatients1();
+//		System.out.println(patients);
+//		System.out.println("Running2");	
+//		String patientsInfo = databaseOps.getAllPatients();
+		for (Patient patient : patients) {
+//			String patientsInfo = String.format("ID: %d, FirstName: %s, Last name: %s, Age: %d, Address: %s, Gender: %s ",
+
+			String patientsInfo = String.format("FirstName: %s, Last name: %s, Age: %d, Address: %s, Gender: %s ",
+					patient.getFName(),
+					patient.getLName(),
+					patient.getAge(),
+					patient.getAddress(),
+					patient.getGender()); 
+			patientsList.addItem(patientsInfo);
+		//	System.out.println(patientsInfo);
+		}
+		}
+	
+	
+		public static void main(String[] args) {
+	        Hospital hospital = new Hospital(new Director("John", "Smith", 58, "Male", "123 Main St"));
+
+	        SwingUtilities.invokeLater(() -> {
+	        	DischargePatientGUI panel = new DischargePatientGUI(hospital);
+	            panel.setVisible(true);
+	        });
+	    
+		
+	}
+	
+	
+	
+	private void removePatient(){
+		String selectedPatient = (String) patientsList.getSelectedItem();
+		if(selectedPatient != null) {
+			String[] parts = selectedPatient.split(", ");
+			if(parts.length >= 2) {
+				
+				String firstname = parts[0].split(": ")[1];
+				String lastname = parts[1].split(": ")[1];
+				int age = Integer.parseInt(parts[2].split(": ")[1]);
+				
+				databaseOps.deletePatient(firstname, lastname, age);
+				populatePatientsComboBox();
+				JOptionPane.showMessageDialog(DischargePatientGUI.this, "Patient discharged successfully.");
+				System.out.println("Patient discharged successfully.");
+
+			}
+			else {
+	            JOptionPane.showMessageDialog(DischargePatientGUI.this, "Invalid format.");
+
+			}
+			
+		}
+		else {
+			dischargeButton.setEnabled(false);
+            JOptionPane.showMessageDialog(DischargePatientGUI.this, "No Patient to discharged.");
+			System.out.println("No patients in the hospital");
+		}
+		
+		
+	}
+	
+	
+
+}
